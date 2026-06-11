5be4cc5461ad68143f0a817a7604623dc2f01f57
saraarteagar91 <sara.arteaga.r91@gmail.com>
2026-06-11 09:02:43 -0500

    Realice ejercicio 1 de pokemon

--- src/main/dosw/semana_2/pokemon/Ejercicio1.java
+++ src/main/dosw/semana_2/pokemon/Ejercicio1.java
@@ -1,1 +1,26 @@
+package main.dosw.semana_2.pokemon;
 
+import java.util.List;
+import java.util.stream.Collectors;
+public class Ejercicio1 {
+
+    record Pokemon(String nombre, String tipo) {}
+
+    public static void main(String[] args) {
+
+        List<Pokemon> pokemones = List.of(
+                new Pokemon("Pikachu",    "Eléctrico"),
+                new Pokemon("Charmander", "Fuego"),
+                new Pokemon("Squirtle",   "Agua"),
+                new Pokemon("Vulpix",     "Fuego"),
+                new Pokemon("Bulbasaur",  "Planta"),
+                new Pokemon("Flareon",    "Fuego")
+        );
+
+        List<String> tipoFuego = pokemones.stream()
+                .filter(p -> p.tipo().equals("Fuego"))
+                .map(Pokemon::nombre)
+                .collect(Collectors.toList());
+        System.out.println("Pokémon tipo Fuego: " + tipoFuego);
+    }
+}
