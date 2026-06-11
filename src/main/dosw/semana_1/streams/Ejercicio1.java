d259ed7d10d7571fa99ab1e5363d348f1a4b2ef3
saraarteagar91 <sara.arteaga.r91@gmail.com>
2026-06-11 08:41:54 -0500

    Realice Ejercicio 1

--- src/main/dosw/semana_1/streams/Ejercicio1.java
+++ src/main/dosw/semana_1/streams/Ejercicio1.java
@@ -1,1 +1,19 @@
+package main.dosw.semana_1.streams;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = List.of(3, 8, 10, 12, 15, 18, 20);
+
+        List<Integer> resultado = numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .filter(n -> n > 10)
+                .collect(Collectors.toList());
+
+        System.out.println("Números pares mayores a 10: " + resultado);
+    }
+}
